15194663316c6fbdb5a41cce72a49d5c7daf0966
K <Redknclarke@gmail.com>
2024-05-15 11:16:20 -0400

    implemented collisions that use mass

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,21 @@ use piston::window::WindowSettings;
 use rand::random;
 
 // Window constants
-const WIDTH: u32 = 600;
-const HEIGHT: u32 = 500;
-const CIRCLE_RADIUS: f64 = 10.0;
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+
+// Circle constants
+const CIRCLE_RADIUS_FACTOR: f64 = 15.0;
+const CIRCLE_RADIUS_MIN: f64 = CIRCLE_RADIUS_FACTOR / 2.0;
 
 // Colors
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
 // World constants
-const GRAVITY: Coordinate = Coordinate { x: 0.0, y: 0.00 };
-// const COLLISION_CONSTANT: f64 = 0.9;
+const GRAVITY: Coordinate = Coordinate { x: 0.0, y: 0.0 };
+const FRICTION: f64 = 0.0;
+const COLLISION_ENERGY_LOSS: f64 = 1.0;
 
 // Structs
 #[derive(Clone, Copy)]
@@ -36,12 +40,20 @@ impl Coordinate {
         self.x += other.x;
         self.y += other.y;
     }
+
+    fn mult(a: &Coordinate, constant: f64) -> Coordinate {
+        Coordinate {
+            x: a.x * constant,
+            y: a.y * constant,
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
 struct Particle {
     pos: Coordinate,
     vel: Coordinate,
+    acc: Coordinate,
     radius: f64,
     mass: f64,
 }
@@ -49,11 +61,11 @@ struct Particle {
 fn main() {
     let mut particles: Vec<Particle> = Vec::new();
 
-    for i in
-        (CIRCLE_RADIUS as u32..(WIDTH - CIRCLE_RADIUS as u32)).step_by(5 * CIRCLE_RADIUS as usize)
+    for i in (CIRCLE_RADIUS_FACTOR as u32..(WIDTH - CIRCLE_RADIUS_FACTOR as u32))
+        .step_by(5 * CIRCLE_RADIUS_FACTOR as usize)
     {
-        for j in (CIRCLE_RADIUS as u32..(HEIGHT - CIRCLE_RADIUS as u32))
-            .step_by(5 * CIRCLE_RADIUS as usize)
+        for j in (CIRCLE_RADIUS_FACTOR as u32..(HEIGHT - CIRCLE_RADIUS_FACTOR as u32))
+            .step_by(5 * CIRCLE_RADIUS_FACTOR as usize)
         {
             let random_x: f64 = random();
             let random_y: f64 = random();
@@ -67,8 +79,9 @@ fn main() {
                     x: random_x,
                     y: random_y,
                 },
-                mass: random_mass,
-                radius: random_mass * CIRCLE_RADIUS + 2.0,
+                acc: Coordinate { x: 0.0, y: 0.0 },
+                mass: random_mass + 1.0,
+                radius: random_mass * CIRCLE_RADIUS_FACTOR + CIRCLE_RADIUS_MIN,
             })
         }
     }
@@ -77,13 +90,13 @@ fn main() {
     let mut window: Window = WindowSettings::new("Particles?", [WIDTH, HEIGHT])
         .opengl(opengl)
         .exit_on_esc(true)
+        .fullscreen(true)
         .build()
         .unwrap();
 
     let e_settings = EventSettings::new();
     let mut gl = GlGraphics::new(opengl);
     let mut events = Events::new(e_settings);
-    let mut frame = 0;
 
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
@@ -99,40 +112,97 @@ fn main() {
                     )
                 }
             });
-            frame += 1;
-            if frame % 1 == 0 {
-                // Update
-                let total_particles: usize = particles.len();
-                for i in 0..total_particles {
-                    let p1 = &mut particles[i];
-                    let vel = &p1.vel;
-                    p1.pos.add(vel);
-                    p1.vel.add(&GRAVITY);
+            // Update
+            let total_particles: usize = particles.len();
+            for i in 0..total_particles {
+                let p1 = &mut particles[i];
 
-                    if p1.pos.x + p1.radius > WIDTH as f64 || p1.pos.x - p1.radius < 0.0 {
-                        p1.vel.x *= -1.0;
+                // Update Kinematics
+                p1.acc = Coordinate::mult(&p1.vel, -FRICTION);
+                p1.acc.add(&GRAVITY);
+                p1.vel.add(&p1.acc);
+                p1.pos.add(&p1.vel);
+
+                if p1.pos.x - p1.radius < 0.0 {
+                    p1.vel.x *= -1.0;
+                    p1.pos.x = p1.radius;
+                } else if p1.pos.x + p1.radius > WIDTH as f64 {
+                    p1.vel.x *= -1.0;
+                    p1.pos.x = WIDTH as f64 - p1.radius;
+                }
+
+                if p1.pos.y - p1.radius < 0.0 {
+                    p1.vel.y *= -1.0;
+                    p1.pos.y = p1.radius;
+                } else if p1.pos.y + p1.radius > HEIGHT as f64 {
+                    p1.vel.y *= -1.0;
+                    p1.pos.y = HEIGHT as f64 - p1.radius;
+                }
+
+                for j in 0..total_particles {
+                    if i == j {
+                        continue;
                     }
+                    // Update Collisions
+                    let pos1 = particles[i].pos;
+                    let pos2 = particles[j].pos;
+                    let dist = ((pos1.x - pos2.x).powi(2) + (pos1.y - pos2.y).powi(2)).sqrt();
+                    if dist < particles[i].radius + particles[j].radius {
+                        let overlap = (dist - particles[i].radius - particles[j].radius) * 0.05;
+                        let corrective_displacement = Coordinate {
+                            x: overlap * (particles[i].pos.x - particles[j].pos.x),
+                            y: overlap * (particles[i].pos.y - particles[j].pos.y),
+                        };
 
-                    if p1.pos.y + p1.radius > HEIGHT as f64 || p1.pos.y - p1.radius < 0.0 {
-                        p1.vel.y *= -1.0;
-                    }
+                        particles[j].pos.add(&corrective_displacement);
+                        particles[i]
+                            .pos
+                            .add(&Coordinate::mult(&corrective_displacement, -1.0));
 
-                    for j in 0..total_particles {
-                        if i == j {
-                            continue;
-                        }
                         let pos1 = particles[i].pos;
                         let pos2 = particles[j].pos;
+
                         let dist = ((pos1.x - pos2.x).powi(2) + (pos1.y - pos2.y).powi(2)).sqrt();
-                        if dist < particles[i].radius + particles[j].radius {
-                            let vi1 = particles[i].vel.clone();
-                            let vi2 = particles[j].vel.clone();
-                            particles[i].vel.x = vi2.x;
-                            particles[j].vel.x = vi1.x;
-                            particles[i].vel.y = vi2.y;
-                            particles[j].vel.y = vi1.y;
-                            break;
-                        }
+
+                        let normal = Coordinate {
+                            x: (pos2.x - pos1.x) / dist,
+                            y: (pos2.y - pos1.y) / dist,
+                        };
+                        let tangent = Coordinate {
+                            x: -normal.y,
+                            y: normal.x,
+                        };
+
+                        let disp_tan_1 =
+                            particles[i].vel.x * tangent.x + particles[i].vel.y * tangent.y;
+                        let disp_tan_2 =
+                            particles[j].vel.x * tangent.x + particles[j].vel.y * tangent.y;
+
+                        let disp_norm_1 =
+                            particles[i].vel.x * normal.x + particles[i].vel.y * normal.y;
+                        let disp_norm_2 =
+                            particles[j].vel.x * normal.x + particles[j].vel.y * normal.y;
+
+                        let m1 = (disp_norm_1 * (particles[i].mass - particles[j].mass)
+                            + 2.0 * particles[j].mass * disp_norm_2)
+                            / (particles[i].mass + particles[j].mass);
+                        let m2 = (disp_norm_2 * (particles[j].mass - particles[i].mass)
+                            + 2.0 * particles[i].mass * disp_norm_1)
+                            / (particles[i].mass + particles[j].mass);
+
+                        particles[i].vel = Coordinate {
+                            x: tangent.x * disp_tan_1 + normal.x * m1,
+                            y: tangent.y * disp_tan_1 + normal.y * m1,
+                        };
+                        particles[j].vel = Coordinate {
+                            x: tangent.x * disp_tan_2 + normal.x * m2,
+                            y: tangent.y * disp_tan_2 + normal.y * m2,
+                        };
+
+                        Coordinate::mult(&particles[i].vel, COLLISION_ENERGY_LOSS);
+                        Coordinate::mult(&particles[j].vel, COLLISION_ENERGY_LOSS);
+
+                        break;
                     }
                 }
             }
